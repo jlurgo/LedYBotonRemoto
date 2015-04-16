743334c1500000f8d130ae6880066759ac8407da
jlurgo <jlurgo@gmail.com>
2015-04-15 22:00:19 -0300

    subida inicial

--- LedYBotonRemoto.js
+++ LedYBotonRemoto.js
@@ -2,14 +2,71 @@ var main = function(){
     var btn_prender = $("#btn_prender");
     var btn_apagar = $("#btn_apagar");
     var led = $("#led");
+
+    var abrirPuertoSerie = function(){
+		serial.open ({baudRate: 115200},
+			function(successMessage) {
+				console.log("puerto serie abierto:", successMessage);
+				
+                btn_prender.click(function(){
+                    serial.write(
+						"1",
+						function(successMessage) {
+							console.log(successMessage);
+						},
+						function(err){
+							console.log("error al enviar por puerto serie:", err);
+						}
+					);	
+                });
+                
+                btn_apagar.click(function(){
+                    serial.write(
+						"0",
+						function(successMessage) {
+							console.log(successMessage);
+						},
+						function(err){
+							console.log("error al enviar por puerto serie:", err);
+						}
+					);	
+                });
+			
+				var buffer_entrada_serie = "";
+				serial.registerReadCallback(
+					function(data){
+						var view = new Uint8Array(data);
+						var recibido = String.fromCharCode.apply(null, view);
+						if(recibido == "1") led.addClass("encendido");
+                        if(recibido == "0") led.removeClass("encendido");
+					},
+					function(err){
+						console.log("error al registrar callback:", err);
+					}
+				);
+			},
+			function(err){
+				console.log("error al abrir puerto serie:", err);
+			}
+		);
+	};
+	
+	serial.requestPermission(
+		 function(successMessage) {
+			console.log("permiso concedido para usar puerto serie:", successMessage);
+			serial.close(function(){
+				console.log("puerto serie cerrado");
+				abrirPuertoSerie();
+			}, function(err){
+				console.log("error al cerrar puerto serie");
+				abrirPuertoSerie();
+			});
+		},
+		function(err){
+			console.log("error al pedir permiso para usar puerto serie:", err);
+		}
+	); 
     
-    btn_prender.click(function(){
-        led.addClass("encendido");
-    });
-    
-    btn_apagar.click(function(){
-        led.removeClass("encendido");
-    });
 };
 
 
